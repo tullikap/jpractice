24d34d2db66c32464f1ccfea2a0f22265b4625bb
Tullika <tullika@gmail.com>
2015-01-24 16:57:28 -0500

    First commit

---
+++ src/com/tullika/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.tullika.helloworld;
+
+/**
+ * Created on 1/24/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
+
